f1f182831a58ecbf795bb37cb46ac0716b03a0e7
Savoia <andrea.savoia2811@gmail.com>
2024-03-04 12:27:51 +0100

    first commit

---
+++ src/main/java/com/sn/budgetbee/BudgetbeeApplication.java
@@ -0,0 +1,13 @@
+package com.sn.budgetbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetbeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sn/budgetbee/BudgetbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sn.budgetbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetbeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
