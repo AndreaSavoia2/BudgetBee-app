6a7ff39a5111ad1d0c9f8ceae0ca5893700a86c5
Savoia <andrea.savoia2811@gmail.com>
2024-03-05 08:50:23 +0100

    commenti

--- src/main/java/com/sn/budgetbee/services/UserServiceImpl.java
+++ src/main/java/com/sn/budgetbee/services/UserServiceImpl.java
@@ -13,25 +13,28 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService{
 
-    private UserDAO userDAO;
-    private PasswordEncoder passwordEncoder;
+    private final UserDAO USER_DAO;
+    private final PasswordEncoder PASSWORD_ENCODER; //necessario alla crypto non copiare nelle altre Implementazioni
 
+    // Iniettore delle dipendenze
     @Autowired
     public UserServiceImpl(UserDAO userDAO){
-        this.userDAO = userDAO;
-        this.passwordEncoder = new BCryptPasswordEncoder();
+        this.USER_DAO = userDAO;
+        this.PASSWORD_ENCODER = new BCryptPasswordEncoder(); //necessario alla crypto non copiare nelle altre Implementazioni
     }
 
+    // Metodo per salvare o modificare un utente già esistente
     @Override
     public User saveUser(User user) {
-        String encodePassword = this.passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodePassword);
-        return userDAO.save(user);
+        String encodePassword = this.PASSWORD_ENCODER.encode(user.getPassword()); //necessario alla crypto non copiare nelle altre Implementazioni
+        user.setPassword(encodePassword); //necessario alla crypto non copiare nelle altre Implementazioni
+        return USER_DAO.save(user);
     }
 
+    // Metodo che restituisce un elemento in base al suo id
     @Override
     public User findUserById(Integer id) {
-        Optional<User> result = userDAO.findById(id);
+        Optional<User> result = USER_DAO.findById(id);
         User user = null;
 
         if(result.isPresent()){
@@ -43,28 +46,31 @@ public class UserServiceImpl implements UserService{
         return user;
     }
 
+    // Metodo che restituisce la lista completa degli elementi della tabella
     @Override
     public List<User> findAllUsers() {
-        return userDAO.findAll();
+        return USER_DAO.findAll();
     }
 
+    // Metodo che elimina un entità dalla tabella in base al suo id
     @Override
     public boolean deleteUserById(Integer id) {
-        Optional<User> result = userDAO.findById(id);
+        Optional<User> result = USER_DAO.findById(id);
 
         if(result.isPresent()){
-            userDAO.deleteById(id);
+            USER_DAO.deleteById(id);
             return true;
         }else{
             throw new RuntimeException("NO ID USER FOUND ERROR: :" + id);
         }
     }
 
+    // Metodi unici per la User Service non sono da copiare nelle altre implementazioni --------------------------------
     @Override
     public boolean checkLogin(String username, String password) {
-        List<User> users = userDAO.findAll();
+        List<User> users = USER_DAO.findAll();
         for (User user: users) {
-            if(user.getUsername().equals(username) && passwordEncoder.matches(password, user.getPassword())){
+            if(user.getUsername().equals(username) && PASSWORD_ENCODER.matches(password, user.getPassword())){
                 return true;
             }
         }
@@ -73,7 +79,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public boolean UsernameIsPresent(String username) {
-        User user = userDAO.findUserByName(username);
+        User user = USER_DAO.findUserByName(username);
         return user == null;
     }
 
