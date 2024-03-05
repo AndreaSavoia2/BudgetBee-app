20e855122387cd4e0e59781a9d97d8593036f47f
Savoia <andrea.savoia2811@gmail.com>
2024-03-05 09:48:31 +0100

    commenti

--- src/main/java/com/sn/budgetbee/controllers/UserController.java
+++ src/main/java/com/sn/budgetbee/controllers/UserController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
-public class UserController implements ControllerInterface<User> {
+@RequestMapping("/api") // infirizzo per accedere alle api
+public class UserController implements ControllerInterface<User>/* L'interfaccia ControllerInterface è un interfaccia con tipi generici condivisa tra le classi controller */{
 
+    //Implementazione della dipendenza con la service
     private final UserService SERVICE;
 
     @Autowired
@@ -18,34 +19,39 @@ public class UserController implements ControllerInterface<User> {
         this.SERVICE = SERVICE;
     }
 
+    // Metodo che richiama il metodo presente nella service per restituite la lista completa della tabella
     @Override
-    @GetMapping("/users")
+    @GetMapping("/users") // l'indirizzo per accedere alla lista (es: host/api/users)
     public List<User> GetAllElements() {
         return SERVICE.findAllUsers();
     }
 
+    // Metodo che richiama il metodo presente nella service per restituite l'elemento corrispondente all'id
     @Override
-    @GetMapping("/users/{userId}")
+    @GetMapping("/users/{userId}") // l'indirizzo per accedere alla lista dove UserId corrisponde all'id nel parametro id (es: host/api/users/2)
     public User GetElementById(@PathVariable("userId") Integer id) {
         return SERVICE.findUserById(id);
     }
 
+    // Metodo che richiama il metodo presente nella service per salvare un nuovo elemento nella tabella
     @Override
     @PostMapping("/users")
     public User SetElement(@RequestBody User user) {
-        user.setId(0);
+        user.setId(0); // si imposta l'id a 0 per far capire al db che si tratta di un utente non esistente
         return SERVICE.saveUser(user);
     }
 
+    // Metodo che richiama il metodo presente nella service per aggiornare un elemento nella tabella
     @Override
     @PutMapping("/users")
     public User UpdateElement(User user) {
         return SERVICE.saveUser(user);
     }
 
+    // Metodo che richiama il metodo presente nella service per eliminare un elemento nella tabella
     @Override
     @DeleteMapping("/users/{userId}")
-    public boolean DeleteElementById(Integer id) {
+    public boolean DeleteElementById(@RequestParam Integer id) {
         return SERVICE.deleteUserById(id);
     }
 }
