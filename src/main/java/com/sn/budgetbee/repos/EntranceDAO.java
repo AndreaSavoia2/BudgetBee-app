0ada9679d9dd35b909f0bd672327a552f7b387f0
Savoia <andrea.savoia2811@gmail.com>
2024-03-06 12:26:00 +0100

    query filtri mese/anno e categoria

--- src/main/java/com/sn/budgetbee/repos/EntranceDAO.java
+++ src/main/java/com/sn/budgetbee/repos/EntranceDAO.java
@@ -1,6 +1,7 @@
 package com.sn.budgetbee.repos;
 
 import com.sn.budgetbee.entities.Entrance;
+import com.sn.budgetbee.entities.Exit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,18 @@ public interface EntranceDAO extends JpaRepository<Entrance, Integer> {
 
     @Query("SELECT e FROM Entrance e JOIN e.budget b WHERE b.id = :budgetId")
     List<Entrance> findEntranceByBudgetId(@Param("budgetId") Integer budgetId);
+
+    @Query("SELECT SUM(e.transaction) as total, e.category " +
+            "FROM Entrance e " +
+            "RIGHT JOIN e.budget b " +
+            "WHERE b.id = :budgetId " +
+            "AND DATE_FORMAT(e.transactionDate, '%m') = :month GROUP BY e.category")
+    List<Entrance> findTotalEntranceByCategoryAndMonth(@Param("budgetId") Integer budgetId, @Param("month") String month);
+
+    @Query("SELECT SUM(e.transaction) as total, e.category " +
+            "FROM Entrance e " +
+            "RIGHT JOIN e.budget b " +
+            "WHERE b.id = :budgetId " +
+            "AND DATE_FORMAT(e.transactionDate, '%Y') = :year GROUP BY e.category")
+    List<Entrance> findTotalEntranceByCategoryAndYear(@Param("budgetId") Integer budgetId, @Param("year") String year);
 }
--- src/main/java/com/sn/budgetbee/repos/ExitDAO.java
+++ src/main/java/com/sn/budgetbee/repos/ExitDAO.java
@@ -12,4 +12,18 @@ public interface ExitDAO extends JpaRepository<Exit, Integer> {
     @Query("SELECT x FROM Exit x JOIN x.budget b WHERE b.id = :budgetId")
     List<Exit> findExitsByBudgetId(@Param("budgetId") Integer budgetId);
 
+    @Query("SELECT SUM(x.transaction) as totale, x.category " +
+            "FROM Exit x " +
+            "RIGHT JOIN x.budget b " +
+            "WHERE b.id = :budgetId " +
+            "AND DATE_FORMAT(x.transactionDate, '%m') = :month GROUP BY x.category")
+    List<Exit> findTotalExitByCategoryAndMonth(@Param("budgetId") Integer budgetId, @Param("month") String month);
+
+    @Query("SELECT SUM(x.transaction) as totale, x.category " +
+            "FROM Exit x " +
+            "RIGHT JOIN x.budget b " +
+            "WHERE b.id = :budgetId " +
+            "AND DATE_FORMAT(x.transactionDate, '%Y') = :year GROUP BY x.category")
+    List<Exit> findTotalExitByCategoryAndYear(@Param("budgetId") Integer budgetId, @Param("year") String year);
+
 }
